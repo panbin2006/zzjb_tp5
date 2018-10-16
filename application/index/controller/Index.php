36d291e24fd4aaaa4d7f6f38cd34de58d0fb24b5
panbin2006 <502752983@qq.com>
2018-10-16 16:32:55 +0800

    增加部门查询模块Scobm

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,12 +1,14 @@
 <?php
 namespace app\index\controller;
 use think\Db;
+use app\
 class Index
 {
-    public function index()
+    public function index($pdate)
     {
     	$result = Db::query("select * from mpplan where pdate>'2018-06-06'");
     	return json($result);
-    	// phpinfo();
+    	phpinfo();
+
     }
 }
---
+++ application/index/controller/Scobm.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace app\index\controller;
+use app\index\model\Scobm as ScobmModel;
+use think\Controller;
+use think\Request;
+
+class Scobm extends Controller
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        $list = ScobmModel::paginate(3);
+        return json($list);
+    }
+
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        //注意表单提交时字段大小写要与数据库字段大小写一致
+        $data = $request->param();
+        $result = ScobmModel::create($data);
+        return json($result);
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  string  $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+       $result = ScobmModel::get($id); 
+       return json($result);
+        // echo 'read';
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  string  $id
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request  $request
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data   = $request->param();
+        $result = ScobmModel::update($data,['BMID' =>$id]);
+        return json($result);
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  string  $carid
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        $result = ScobmModel::destroy($id);
+        return json($result);
+    }
+}
